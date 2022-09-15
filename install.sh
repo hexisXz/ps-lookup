873546fcab68bae0a92ab500e0d22a85028f180f
hexisXz <qk371994@gmail.com>
2022-09-14 21:51:35 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -9,25 +9,19 @@
 
 
 
-echo "would you like to install ps-lookup (y/n)"
 
-
-read r
-
-if [ $r == "y" ]; then
-       echo "installing . . ."
-       git clone https://github.com/hexisXz/ps-lookup.git && mv ~/ps-lookup ~/ps-lookup- && sudo mv ps-lookup-/ps-lookup /bin && chmod +x /bin/ps-lookup
-      
- 
-fi
-
-
-
-if [ $r == "n" ]; then
-	echo "exiting..."
+if [ whiptail --title "install" --yesno "would you like to install ps-lookup?" 10 60 ]; then
+	 echo "installing . . ."
+         git clone https://github.com/hexisXz/ps-lookup.git && mv ~/ps-lookup ~/ps-lookup- && sudo mv ps-lookup-/ps-lookup /bin && chmod +x /bin/ps-lookup
+else
         exit
 
-fi
+
+
+
+
+
+
 
 
 
