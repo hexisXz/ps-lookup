2f3c2f939c491918accb89e177ec0ae35381c3a0
hexisXz <qk371994@gmail.com>
2022-09-22 21:10:20 -0400

    install

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@
 # C R E A T E D   BY   H E X I S #
 ##################################
 
-
-git clone https://github.com/hexisXz/ps-lookup.git && mv ~/ps-lookup ~/ps-lookup- && sudo mv ps-lookup-/ps-lookup /bin && chmod +x /bin/ps-lookup
-
+if (whiptail --title "install" --yesno "do you want to install ps-lookup?" 10 60) then
+	git clone https://github.com/hexisXz/ps-lookup.git && mv ~/ps-lookup ~/ps-lookup- && sudo mv ps-lookup-/ps-lookup /bin && chmod +x /bin/ps-lookup
+else
+   exit 1
 
 
