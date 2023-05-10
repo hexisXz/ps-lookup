0d33af72eb366900db5687a5ab940c604ca378b5
hexis <71829613+hexisXz@users.noreply.github.com>
2023-05-10 13:09:02 -0400

    Update install.sh
    fixed ps-lookup not installing.

--- install.sh
+++ install.sh
@@ -9,10 +9,7 @@
 # C R E A T E D   BY   H E X I S #
 ##################################
 
-if (whiptail --title "install" --yesno "do you want to install ps-lookup?" 10 60) then
-	git clone https://github.com/hexisXz/ps-lookup.git && mv ~/ps-lookup ~/ps-lookup- && sudo mv ps-lookup-/ps-lookup /bin && chmod +x /bin/ps-lookup
-else
-   exit 1
-fi
+git clone https://github.com/hexisXz/ps-lookup.git && sudo mv ps-lookup-/ps-lookup /bin && chmod +x /bin/ps-lookup
+
 
 
