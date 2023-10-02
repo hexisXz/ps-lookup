76792809f254c065bf9b8d0c3aeb8fcf7f97e70c
hexis <71829613+hexisXz@users.noreply.github.com>
2023-10-02 12:26:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 # C R E A T E D   BY   H E X I S #
 ##################################
 
-git clone https://github.com/hexisXz/ps-lookup.git && sudo mv ./ps-lookup/ps-lookup /bin && chmod +x /bin/ps-lookup
+git clone https://github.com/hexisXz/ps-lookup.git && sudo mv ps-lookup/ps-lookup /bin && chmod +x /bin/ps-lookup
 
 
 
